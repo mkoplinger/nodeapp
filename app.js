01e0d1cbd836cd7a633403a5308ef2e1b431055b
mkoplinger <42058972+mkoplinger@users.noreply.github.com>
2021-04-11 18:31:17 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
+const express = require('express')
+// const https = require('https')
+const http = require('http')
+const fs = require('fs');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+fs.mkdirSync('./logs', { recursive: true });
+
+app.get('/', (req, res) => {
+  fs.appendFileSync('./logs/log', 'ddfdfds');
+  res.send('Hello World!')
+})
+
+// const server = https.createServer({ cert: fs.readFileSync('./fullchain.pem'), key: fs.readFileSync('./privatekey.pem') }, app);
+const server = http.createServer({}, app);
+
+// server.listen(443, () => console.log('App listening.'));
+server.listen(8080, () => console.log('App listening.'));
